f2d60fdd7a8ab269f7c709ee68f95428798b0f62
GreyCat20142017 <nrz3siaatg81@mail.ru>
2018-08-19 23:25:26 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './components/Game';
-
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Game />, document.getElementById('root'));
-registerServiceWorker();
